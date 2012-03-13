a45f6b595c8cbdd9979672f16d386b8a79e0cba1
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-13 16:06:49 -0500

    Added initial LL insertion code to handle head and current nodes

--- linked_list.c
+++ linked_list.c
@@ -6,11 +6,26 @@
 int add_new_node(int);
 int print_out_ll(struct node *);
 
+item *head = NULL;
+
 int add_new_node(int data) {
 	
-	item *head, *curr;
+	item *curr;
 
-	
+	/* We haven't started the LL...*/
+	if (head == NULL) {
+		head = (struct node *)malloc(sizeof(struct node));
+		head->data=data;
+		head->next=NULL;
+		return 0;
+	}
+	else
+	{
+		curr = (struct node *)malloc(sizeof(struct node));
+		curr->data=data;
+		head->next=curr;
+		head=curr;
+	}
 
 	return 0;
 }
