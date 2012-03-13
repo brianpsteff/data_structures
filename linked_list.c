a06ff6c16401166ed0ad604bd692f944e91e78bc
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-13 15:58:12 -0500

    Added function definitions

--- linked_list.c
+++ linked_list.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <linked_list.h>
+
+int add_new_node(int);
+int print_out_ll(struct node *);
+
+int add_new_node(int data) {
+	
+	item *head, *curr;
+
+	
+
+	return 0;
+}
