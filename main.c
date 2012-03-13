56bfdff763490a475dd4b61294c575a6ac5e7883
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-13 15:56:37 -0500

    Added initial test array

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* main.c is a simple test program to excerise various DS's*/
+
 int main(int argc, char **argv) {
 
-return 0;
+	int storeUs[5] = {5,10,15,7,2};
+
+	
+
+	return 0;
 }
