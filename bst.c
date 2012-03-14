af0538390da4ccb44b435295dc33dc5c5a90db48
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-14 13:18:52 -0500

    Added initial add new node function template

--- bst.c
+++ bst.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <bst.h>
+
+extern void add_new_node_bst(int);
+
+struct node *root;
+struct node *temp;
+
+extern void add_new_node_bst(int data) {
+
+
+}
