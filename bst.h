8ed877e009d3f437caa8926a7581037a5ffd7765
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-14 14:26:56 -0500

    Moved function prototypes to header and renamed struct

--- bst.h
+++ bst.h
@@ -1,5 +1,10 @@
-struct node {
+struct bst_node {
 	int data;
-	struct node *left;
-	struct node *right;
+	struct bst_node *left;
+	struct bst_node *right;
 };
+
+extern void add_new_node_bst(int);
+extern void print_out_bst_breadth(struct bst_node *);
+extern struct bst_node * get_root();
+void recurse_bst_add(struct bst_node *,int);
