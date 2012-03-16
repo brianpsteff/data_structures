6e14d2d1b9d8bcf23796a6379171917f783776ef
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-16 01:04:23 -0500

    Added function prototype for searching the bst

--- bst.h
+++ bst.h
@@ -8,3 +8,4 @@ extern void add_new_node_bst(struct bst_node *,int);
 extern void print_out_bst_breadth(struct bst_node *);
 extern void remove_node_bst(struct bst_node *, int);
 void recurse_bst_add(struct bst_node *,int);
+extern int search_bst(struct bst_node *,int);
