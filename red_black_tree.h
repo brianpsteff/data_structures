fe2544636113d7ed697841db4c5d999bc8561e18
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-21 18:26:17 -0500

    Modified function prototypes

--- red_black_tree.h
+++ red_black_tree.h
@@ -1,8 +1,11 @@
 struct red_black_node {
 	int data;
+	int key;
 	int color;
 	struct red_black_node *left;
 	struct red_black_node *right;
+	struct red_black_node *parent;
 };
 
+extern struct red_black_node * create_rbt();
 extern void insert_node_rbt(struct red_black_node *, int);
