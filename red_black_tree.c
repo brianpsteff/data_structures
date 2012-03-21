54c608289703cad318799126b8204e184c95c818
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-21 18:25:49 -0500

    Additional insertion code for RBTs

--- red_black_tree.c
+++ red_black_tree.c
@@ -3,8 +3,25 @@
 
 #include <red_black_tree.h>
 
-extern void insert_node_rbt(struct red_black_node *node, int data) {
+/*
+ * node->color = 1 => black
+ * node->color = 0 => red
+ */
 
+extern struct red_black_node* create_rbt() {
 
-return;
+	//Hey we have a new rb tree;
+	struct red_black_node *temp;
+	temp=(struct red_black_node *)malloc(sizeof(struct red_black_node));
+	//temp->data=data;
+	temp->color=1;
+	temp->left=NULL;
+	temp->right=NULL;
+	
+	return(temp);
+}
+
+extern void insert_node_rbt(struct red_black_node *root, int data) {
+	
+
 }
