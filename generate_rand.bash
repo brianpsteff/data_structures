8b346238e602c36a6b8d86d20c99a79c2764e428
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-23 18:36:26 -0500

    BASH script to generate a set of random numbers for testing

---
+++ generate_rand.bash
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Simple BASH script to generate a file of 10000 random numbers for testing #
+
+if [ -a ./rand_keys.txt ]; then
+	rm rand_keys.txt
+fi
+
+if [ -a ./rand_data.txt ]; then
+	rm rand_data.txt
+fi
+
+for i in {1..100}; do
+	echo $RANDOM >> rand_keys.txt
+done;
+
+for j in {1..100}; do
+	echo $RANDOM >> rand_data.txt
+done;
