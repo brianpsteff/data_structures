3a867fdc24c015983d92e227e5e3687faf953fb9
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-24 16:30:04 -0500

    Added to function prototype to handle counting occurences of element in LL

--- linked_list.h
+++ linked_list.h
@@ -9,3 +9,4 @@ void remove_node(struct node *, int);
 void print_out_ll(struct node *);
 struct node * create_new_ll();
 int total_ll_elements(struct node *);
+int find_elements_ll(struct node *,int);
