c884cf96293a759256b9b66d7848e8c1c701b8a1
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-24 16:18:36 -0500

    Changed some function prototypes to return pointers to structures and added new prototype to handle LL counting

--- linked_list.h
+++ linked_list.h
@@ -3,7 +3,9 @@ struct node {
 	struct node *next;
 };
 
-extern void add_new_node(struct node *, int);
-extern void remove_node(struct node *, int);
-extern void print_out_ll(struct node *);
-
+/*External functions*/
+void add_new_node(struct node *, int);
+void remove_node(struct node *, int);
+void print_out_ll(struct node *);
+struct node * create_new_ll();
+int total_ll_elements(struct node *);
