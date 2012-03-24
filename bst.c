bee0c64aff76a7e5246a45492054b8f3744a050a
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-03-24 17:19:50 -0500

    removed recurse_add function and just placed the recursive addition in a single function. Also added simple bst root initaliziation function

--- bst.c
+++ bst.c
@@ -3,86 +3,78 @@
 
 #include <bst.h>
 
-//struct bst_node *root;
-struct bst_node *temp;
+/* Simple function to initalize a new BST */
+extern struct bst_node * create_new_bst() {
+	struct bst_node *root = (struct bst_node *)malloc(sizeof(struct bst_node));
+	return root;
+}
 
 extern void add_new_node_bst(struct bst_node *root, int data) {
 	
-	//This is a hack; Need to change to if(root->data is not defined)
+	struct bst_node *new = NULL;
+
 	if(root->data == -1) {
 		root->data = data;
 		root->left = NULL;
 		root->right = NULL;
-		temp=root;
 	}
 	else {
-		recurse_bst_add(temp,data);
-		temp=root;
-	}
-}
-
-void recurse_bst_add(struct bst_node *present, int data) {
-
-	struct bst_node *new;	
-	
-	if(data < present->data) {
-		if(present->left != NULL) {
-			present=present->left;
-			recurse_bst_add(present,data);
-		}
-		else {
-			new=(struct bst_node *)malloc(sizeof(struct bst_node));
-			present->left=new;
-			new->data=data;
-			new->left=NULL;
-			new->right=NULL;
-		}
-	}
-	else
-	{
-    if(present->right != NULL) {
-      present=present->right;
-      recurse_bst_add(present,data);
+	if(data < root->data) {
+    if(root->left != NULL) {
+      root=root->left;
+      add_new_node_bst(root,data);
     }
     else {
       new=(struct bst_node *)malloc(sizeof(struct bst_node));
-      present->right=new;
+      root->left=new;
       new->data=data;
       new->left=NULL;
       new->right=NULL;
     }
-	}
+  }
+  else
+  {
+    if(root->right != NULL) {
+      root=root->right;
+      add_new_node_bst(root,data);
+    }
+    else {
+      new=(struct bst_node *)malloc(sizeof(struct bst_node));
+      root->right=new;
+      new->data=data;
+      new->left=NULL;
+      new->right=NULL;
+    }
+  }
+ }
 }
 
 extern void remove_node_bst(struct bst_node *root, int data) {
 	
-	//temp=root;
 	int holder;
 	
 	if(root->data==data) {
 	/*Okay we found the node, get the logical successor*/
-	temp=root;
 		
-		if(temp->right==NULL) {
-			if(temp->left==NULL) {
+		if(root->right==NULL) {
+			if(root->left==NULL) {
 				free(root);
 			}
 			else {
-				holder=temp->left->data;
-				temp->left->data=root->data;
+				holder=root->left->data;
+				root->left->data=root->data;
 				root->data=holder;
 				remove_node_bst(root->left,data);
 			}
 		}
 		else {
-			holder=temp->right->data;
-			temp->right->data=root->data;
+			holder=root->right->data;
+			root->right->data=root->data;
 			root->data=holder;
 			remove_node_bst(root->right,data);
 		}
-
 	}
-	else if(data<root->data)
+	else if(data<root->data) //Go left...
 	{	
 		remove_node_bst(root->left,data);
 	}	
@@ -92,15 +84,16 @@ extern void remove_node_bst(struct bst_node *root, int data) {
 	}
 }
 
-extern void print_out_bst_breadth(struct bst_node *temp) {
-	if(temp == NULL) 
+extern void print_out_bst_breadth(struct bst_node *root) {
+	if(root == NULL) 
 		return;
-	fprintf(stdout,"%d\n",temp->data);
-	print_out_bst_breadth(temp->left);	
-	print_out_bst_breadth(temp->right);	
+	fprintf(stdout,"%d\n",root->data);
+	print_out_bst_breadth(root->left);	
+	print_out_bst_breadth(root->right);	
 }
 
 extern int search_bst(struct bst_node *root, int data) {
 
-	return 0;
+	return 2;
 }
+
