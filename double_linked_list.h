d3359d5f15b02b8eba3c613e1578d18aa5084cdc
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-04-03 16:46:42 -0500

    Added new function prototypes

--- double_linked_list.h
+++ double_linked_list.h
@@ -9,4 +9,4 @@ struct double_node * create_new_dll();
 void add_new_node_dll(struct double_node *, int);
 void remove_node_dll(struct double_node *, int);
 void print_out_dll(struct double_node *);
-
+void print_forward_back_dll(struct double_node *, int, int);
