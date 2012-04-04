0aaaf1d2784d4d398779d228d85c3c57e066ff21
Matthew Piehl <Matthew.Piehl@ndsu.edu>
2012-04-04 15:20:41 -0500

    Added list head structure which contains reference to the actual linked list.

--- linked_list.c
+++ linked_list.c
@@ -4,26 +4,25 @@
 #include <linked_list.h>
 
 /* Simple function to initalize a new LL */
-extern struct node * create_new_ll() {
-	struct node *head;
-	head=(struct node *)malloc(sizeof(struct node));
+extern struct list_head * create_new_ll() {
+	struct list_head *head;
+	head=(struct list_head *)malloc(sizeof(struct list_head));
+	head->start=(struct node *)malloc(sizeof(struct node));
 	return head;
 }
 
 /* Responsible for adding new elements to the LL */
-extern void add_new_node_ll(struct node *head, int data) {
+extern void add_new_node_ll(struct list_head *head, int data) {
 
-	struct node *temp = NULL;
+	struct node *temp = head->start;
 
 	/* We haven't started the LL...*/
-	if (head->next == NULL) {
-		temp = (struct node *)malloc(sizeof(struct node));
-		head->data=data;
-		head->next=temp;
+	if (temp->next == NULL) {
+		temp->next = (struct node *)malloc(sizeof(struct node));
+		temp->data=data;
 	}
 	else
 	{
-		temp = head;
 		while(temp->next != NULL) {
 			temp=temp->next;
 		}
@@ -34,41 +33,33 @@ extern void add_new_node_ll(struct node *head, int data) {
 }
 
 /* Remove a single entry from the LL */
-extern void remove_node_ll(struct node *head, int data) {	
+extern void remove_node_ll(struct list_head *head, int data) {	
 
-	struct node *temp = NULL;
-
-	temp = head;
+	struct node *temp = head->start;
+	struct node *hold = NULL;
 
 	/*Head node has to be deleted*/
 	if(temp->data == data) {
-		temp = temp->next;
-		head=temp;
+		head->start=temp->next;
+		free(temp);
 	}
 	/*Loop and check all other nodes*/
+	hold=temp;
 	while(temp->next != NULL) {
-		if(temp->next->data == data)
-		{
-			if(temp->next->next != NULL) {
-				free(temp->next);
-				temp->next=temp->next->next;
-			}
-			else {
-				free(temp->next);
-				temp->next=NULL;
-				continue;
-			}
+		if(temp->data==data) {
+			hold->next=temp->next;
+			free(temp);
+			return;
 		}
+		hold=temp;
 		temp=temp->next;
 	}
 }
 
 /*Print out the current structure of the LL */
-extern void print_out_ll(struct node *head) {	
+extern void print_out_ll(struct list_head *head) {	
 
-	struct node *temp = NULL;
-
-	temp = head;
+	struct node *temp = head->start;
 
 	fprintf(stdout, "Current linked list\n\n\t");
 
@@ -81,27 +72,24 @@ extern void print_out_ll(struct node *head) {
 }
 
 /*Return the total elements in the LL*/
-extern int total_elements_ll(struct node *head) {
+extern int total_elements_ll(struct list_head *head) {
 
 	int total = 0;
-	struct node *temp = NULL;
-	temp = head;
+	struct node *temp = head->start;
 
 	while(temp->next != NULL) {
 		total++;
 		temp=temp->next;
 	}
-	total++;
 
 	return total;
 }
 
 /*Find and return the total occurences of an element*/
-extern int find_elements_ll(struct node *head, int data) {
+extern int find_elements_ll(struct list_head *head, int data) {
 	int total = 0;
 
-	struct node *temp = NULL;
-	temp = head;
+	struct node *temp = head->start;
 
 	while(temp->next != NULL) {
 		if(temp->data==data)
@@ -119,11 +107,9 @@ extern int find_elements_ll(struct node *head, int data) {
  * return 0 if no occurence; Better best-case complexity compared to
  * find_element_ll()
  */
-extern int does_element_exist_ll(struct node *head, int data) {
-	struct node *temp = NULL;
+extern int does_element_exist_ll(struct list_head *head, int data) {
+	struct node *temp = head->start;
 	
-	temp=head;
-
 	while(temp->next != NULL) {
 		if(temp->data==data)
 			return 1;
@@ -138,9 +124,24 @@ extern int does_element_exist_ll(struct node *head, int data) {
 }
 
 /* Is a list empty? Return 1 if true; 0 else */
-int is_list_empty(struct node *head) {
-	if(head->next == NULL) {
+int is_list_empty(struct list_head *head) {
+	struct node *temp = head->start;
+	if(temp->next == NULL) {
 		return 1;
 	}	
 	return 0;
+}
+
+void free_ll(struct list_head *head) {
+	struct node *temp = head->start;
+	struct node *hold = NULL;
+
+	while(temp->next != NULL) {
+		hold=temp;
+		free(hold);
+		temp=temp->next;
+	}
+	//free(temp);
+	free(head);
+	printf("List Free!\n");
 }
--- linked_list.h
+++ linked_list.h
@@ -3,12 +3,17 @@ struct node {
 	struct node *next;
 };
 
+struct list_head {
+	struct node *start;
+};
+
 /*External functions*/
-void add_new_node_ll(struct node *, int);
-void remove_node_ll(struct node *, int);
-void print_out_ll(struct node *);
-int total_elements_ll(struct node *);
-int find_elements_ll(struct node *,int);
-int does_element_exist_ll(struct node *,int);
-struct node * create_new_ll();
-int is_list_empty(struct node *);
+void add_new_node_ll(struct list_head *, int);
+void remove_node_ll(struct list_head *, int);
+void print_out_ll(struct list_head *);
+int total_elements_ll(struct list_head *);
+int find_elements_ll(struct list_head *,int);
+int does_element_exist_ll(struct list_head *,int);
+struct list_head * create_new_ll();
+int is_list_empty(struct list_head *);
+void free_ll(struct list_head *);
